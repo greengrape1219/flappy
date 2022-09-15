b507a4ab6d272b5c174cb04dfa7c8fdcff83d9bd
greengrape1219 <113372359+greengrape1219@users.noreply.github.com>
2022-09-15 11:28:14 +0900

    day4

---
+++ Assets/MakePipe.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakePipe : MonoBehaviour
+{
+    public GameObject pipe;
+    public float timeDiff;
+        float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer>timeDiff)
+        { GameObject newpipe= Instantiate(pipe); //pipe 생성하는 newpipe 변수
+            newpipe.transform.position = new Vector3(6, Random.Range(-1.25f,3.25f), 0); //생성되는 파이프의 위치 변하도록
+            timer = 0;
+            Destroy(newpipe, 10.0f); // 몇 초뒤에 파이프가 사라지도록
+        }
+        
+    }
+}
--- Assets/move.cs
+++ Assets/move.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +14,7 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.left; //(-1,0,0)
-        Debug.Log(transform.position);
+        transform.position += Vector3.left*speed*Time.deltaTime ; //(-1,0,0)transform이 세개의 벡터를 가지기에
+        //퍼블릭인 스피드 변수로 조절. 델타타임으로 프레임이 다른 사용자간 속도를 맞춤.
     }
 }
